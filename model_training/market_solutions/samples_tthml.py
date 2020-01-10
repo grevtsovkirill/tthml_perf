b6b5e30fbc8b89b6fe5e85f044f608aefe056d49
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-01-10 16:04:41 +0100

    update sample details

--- model_training/market_solutions/samples_tthml.py
+++ model_training/market_solutions/samples_tthml.py
@@ -1,12 +1,12 @@
 LUMI = 35900 # in pb
 
 ###
-BASE       = 'ttH_Multilep/'
+BASE       = '../../Files/ttH_Multilep/'
 
 ####
 samples = {    
     'ttH' : {
-        'filename' : 'mc16_13TeV.aMcAtNloPythia8EvtGen_ttH_r9364_p3832.root',   
+        'filename' : 'mc16_13TeV.aMcAtNloPythia8EvtGen_ttH_r9364_p3832.csv',   
         'xsec'     : None,   
         'eff'      : 1.,   
         'kfactor'  : 1.,   
@@ -14,18 +14,19 @@ samples = {
         'color'    : 'black',   
     },
     'ttW' : {
-        'filename' : 'mc16_13TeV.Sherpa_221_NN30NNLO_ttW_multilegNLO_r9364_p3830.root',   
+        'filename' : 'mc16_13TeV.Sherpa_221_NN30NNLO_ttW_multilegNLO_r9364_p3830.csv',   
         'xsec'     : 580,   
         'eff'      : 1.,   
         'kfactor'  : 1.,   
         'weight'   : 1.,   
         'color'    : 'orange',   
     },
-    'ttbar' : {
-        'filename' : 'mc16_13TeV.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad_r9364_p3980.roott',   
-        'xsec'     : 47.2,   
-        'eff'      : 1.,   
-        'kfactor'  : 1.,   
-        'weight'   : 1.,   
-        'color'    : 'green',   
-    },
+    # 'ttbar' : {
+    #     'filename' : 'mc16_13TeV.PhPy8EG_A14_ttbar_hdamp258p75_nonallhad_r9364_p3980.roott',   
+    #     'xsec'     : 47.2,   
+    #     'eff'      : 1.,   
+    #     'kfactor'  : 1.,   
+    #     'weight'   : 1.,   
+    #     'color'    : 'green',   
+    # }
+}
