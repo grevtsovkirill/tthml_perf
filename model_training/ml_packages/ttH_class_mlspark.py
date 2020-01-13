89b5888def3d7d1da1ad28e7e35444d024270e59
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-01-13 13:25:21 +0100

    load data to spark sql session

--- model_training/ml_packages/ttH_class_mlspark.py
+++ model_training/ml_packages/ttH_class_mlspark.py
@@ -1,16 +1,34 @@
 
 import sys, math
- 
+
+import pyspark.sql
 from pyspark import SparkContext, SparkConf
 from pyspark.sql.functions import lit
 from samples_tthml import *
 
+def data_load(in_list):
+    DFList = [] 
+    label_count=0
+    for s in samples:
+        dsPath = BASE+samples[s]['filename']
+        print('Loading {0} sample file'.format(s),'\n',dsPath )   
+        tempDF =session.read\
+                          .format('csv')\
+                          .option('header', 'true')\
+                          .option('inferschema', 'true')\
+                          .load(dsPath)
+        tempDF=tempDF.withColumn('sample', lit(s)) 
+        tempDF=tempDF.withColumn('label', lit(label_count)) 
+        label_count+=1
+        DFList.append(tempDF)
+    return DFList
+
 def main():
-    # create Spark context with Spark configuration
-    conf = SparkConf().setAppName("Train ttH classifier")
-    sc = SparkContext(conf=conf)
 
     for s in samples:
         print(BASE,samples[s]['filename'])
+    data_load(samples)
+        
 if __name__ == "__main__":
+    session = pyspark.sql.SparkSession.builder.appName("Train ttH classifier").getOrCreate()
     main()
