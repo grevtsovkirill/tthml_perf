b052699d775a69124da5ed70464a1d32fd37061a
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-01-13 13:14:47 +0100

    import da

--- model_training/ml_packages/ttH_class_mlspark.py
+++ model_training/ml_packages/ttH_class_mlspark.py
@@ -2,9 +2,15 @@
 import sys, math
  
 from pyspark import SparkContext, SparkConf
- 
+from pyspark.sql.functions import lit
+from samples_tthml import *
+
+def main():
+    # create Spark context with Spark configuration
+    conf = SparkConf().setAppName("Train ttH classifier")
+    sc = SparkContext(conf=conf)
+
+    for s in samples:
+        print(BASE,samples[s]['filename'])
 if __name__ == "__main__":
- 
-  # create Spark context with Spark configuration
-  conf = SparkConf().setAppName("Train ttH classifier")
-  sc = SparkContext(conf=conf)
+    main()
