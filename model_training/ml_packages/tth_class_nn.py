75aab704a0f283c654393fa972feedd4802dd6c4
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-04-04 21:38:37 +0200

    load data into df

--- model_training/ml_packages/tth_class_nn.py
+++ model_training/ml_packages/tth_class_nn.py
@@ -6,12 +6,16 @@ import json
 from samples_tthml import *
 
 def data_load(in_list):
+    df = {}
     for s in samples:
-        print(samples[s]['filename'])
-
+        print(s,'  ',samples[s]['filename'])
+        df[s] = pd.read_csv(BASE+samples[s]['filename'])
+    return df
+    
 def main():
 
     dfs=data_load(samples)
-
+    print(dfs['ttH'].head())
+    
 if __name__ == "__main__":
     main() 
