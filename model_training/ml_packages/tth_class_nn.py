76ec0db69626feead99b1fda68afd05839e62062
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-04-04 21:31:07 +0200

    main structure. load samples info

--- model_training/ml_packages/tth_class_nn.py
+++ model_training/ml_packages/tth_class_nn.py
@@ -2,3 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+
+from samples_tthml import *
+
+def data_load(in_list):
+    for s in samples:
+        print(samples[s]['filename'])
+
+def main():
+
+    dfs=data_load(samples)
+
+if __name__ == "__main__":
+    main() 
