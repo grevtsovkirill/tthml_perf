17cb2976ed875574ab11325225a9990257f18f7d
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2020-04-04 21:46:12 +0200

    choose correct features

--- model_training/ml_packages/tth_class_nn.py
+++ model_training/ml_packages/tth_class_nn.py
@@ -5,11 +5,21 @@ import json
 
 from samples_tthml import *
 
-def data_load(in_list):
+def sel_vars(list_name="../VarList_30.json"):
+    with open(list_name) as vardict:
+        variablelist = json.load(vardict)[:]    
+    return variablelist
+
+def data_load(in_list, do_clean=True):
     df = {}
+    if do_clean:
+        var_list=sel_vars()
+
     for s in samples:
         print(s,'  ',samples[s]['filename'])
         df[s] = pd.read_csv(BASE+samples[s]['filename'])
+        if do_clean:
+            df[s] = df[s][var_list]
     return df
     
 def main():
